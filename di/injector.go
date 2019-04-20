77ff04fac31f8272276e07e213c0457d9308e81c
hiroaki <hiroaki.sekine@groove-x.com>
2019-04-20 18:26:20 +0900

    implement tracer

--- di/injector.go
+++ di/injector.go
@@ -1,12 +1,14 @@
 package di
 
 import (
+	"awesomeProject1/trace"
 	"awesomeProject1/user"
 	"awesomeProject1/user/delivery"
 	"awesomeProject1/user/delivery/echo"
 	"awesomeProject1/user/repository"
 	"awesomeProject1/user/repository/sqlite3"
 	"database/sql"
+	"os"
 )
 
 func InjectDB() *sql.DB {
@@ -18,12 +20,16 @@ func InjectDB() *sql.DB {
 	return db
 }
 
+func InjectTracer() trace.Tracer {
+	return trace.New(os.Stdout)
+}
+
 func InjectUserRepository() repository.UserRepository {
 	return sqlite3.NewUserRepository(InjectDB())
 }
 
 func InjectUserUsecase() user.UserUsecase {
-	return user.NewUserUsecase(InjectUserRepository())
+	return user.NewUserUsecase(InjectUserRepository(), InjectTracer())
 }
 
 func InjectUserDelivery() delivery.UserDelivery {
--- user/user.go
+++ user/user.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"awesomeProject1/model"
+	"awesomeProject1/trace"
 	"awesomeProject1/user/repository"
 )
 
@@ -14,29 +15,29 @@ type UserUsecase interface {
 
 type userUsecase struct {
 	userRepo repository.UserRepository
+	tracer   trace.Tracer
 }
 
-func NewUserUsecase(userRepo repository.UserRepository) UserUsecase {
-	return userUsecase{userRepo}
+func NewUserUsecase(userRepo repository.UserRepository, tracer trace.Tracer) UserUsecase {
+	return userUsecase{userRepo, tracer}
 }
 
 func (uu userUsecase) Get(userID int) (*model.User, error) {
+	uu.tracer.Trace("Get User")
 	return uu.userRepo.Get(userID)
 }
 
 func (uu userUsecase) Add(user *model.User) (*model.User, error) {
+	uu.tracer.Trace("Add User")
 	return uu.userRepo.Add(user)
 }
 
 func (uu userUsecase) Edit(user *model.User) error {
+	uu.tracer.Trace("Edit User")
 	return uu.userRepo.Edit(user)
 }
 
 func (uu userUsecase) Delete(userID int) error {
+	uu.tracer.Trace("Delete User")
 	return uu.userRepo.Delete(userID)
 }
-
-
-
-
-
